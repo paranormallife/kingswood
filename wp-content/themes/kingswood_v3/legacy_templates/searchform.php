70cb527a7656525e82717d31e120cc6886f6fe60
elliottoman <elliottoman@gmail.com>
2019-05-25 15:47:02 -0400

    Security updates

--- wp-content/themes/kingswood_v3/legacy_templates/searchform.php
+++
@@ -1,5 +0,0 @@
-<!-- Search Form -->
-
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-<input type="text" name="s" id="s" placeholder="search:" />
-</form>
